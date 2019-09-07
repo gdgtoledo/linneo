a4c4e8a68f9268e00cb8bc00607fd56c97163007
Manuel de la Peña <mdelapenya@gmail.com>
2019-09-07 12:29:27 +0200

    (#5) Create a search on elasticsearch with no filters

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"net/http"
 
+	"github.com/gdgtoledo/linneo/dao"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.GET("/plants", func(c *gin.Context) {
+		res, err := dao.Search("plants", map[string]interface{}{})
+		log.WithFields(log.Fields{
+			"result": res,
+		}).Info("Query Result")
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+			}).Error("Error querying database")
+		}
 		c.String(http.StatusNoContent, "There are no plants in the primary storage")
 	})
 
