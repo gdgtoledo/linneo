645ed68637bde51fc18a6b723d868da5a0dd70c6
Javier López de Ancos <javierlopezdeancos@outlook.com>
2019-10-29 17:42:57 +0100

    [#7] add delete handler and refactor syntax for search handler

--- main.go
+++ main.go
@@ -3,28 +3,54 @@ package main
 import (
 	"net/http"
 
-	"github.com/gdgtoledo/linneo/dao"
+	"github.com/gdgtoledo/linneo/plants"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
-func searchPlants(c *gin.Context) {
-	res, err := dao.Search("plants", map[string]interface{}{})
+var routes = map[string]string{"plants": "/plants", "plant": "/plants/:id"}
+
+func handleSearchItems(c *gin.Context) {
+	searchQueryByIndexName := plants.SearchQueryByIndexName{
+		IndexName: "plants",
+		Query:     map[string]interface{}{},
+	}
+
+	res, err := plants.Search(searchQueryByIndexName)
+
 	log.WithFields(log.Fields{
 		"result": res,
 	}).Info("Query Result")
+
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
 		}).Error("Error querying database")
 	}
+
 	c.String(http.StatusNoContent, "There are no plants in the primary storage")
+}
+
+func handleDeleteItem(c *gin.Context) {
+	var plant plants.Model
+	result, err := plants.Delete(plant.ID)
+
+	log.WithFields(log.Fields{
+		"result": result,
+	}).Info("Delete Query Result")
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Error("Error deleting a plant")
+	}
 }
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/plants", searchPlants)
+	r.GET(routes["plants"], handleSearchItems)
+	r.DELETE(routes["plant"], handleDeleteItem)
 
 	return r
 }
